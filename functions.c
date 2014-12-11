809fe9640bdce89bbc39c16a0553580c85e4da33
ClaytonJaksha <c16clayton.jaksha@usafa.edu>
2014-12-11 13:04:40 -0700

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,73 @@
+/*
+ * functions.c
+ *
+ *  Created on: Nov 14, 2014
+ *      Author: c16clayton.jaksha
+ */
+#include <msp430g2553.h>
+#include "functions.h"
+void moveRobotForward(void)
+{
+	TA1CCR1 = FULL_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_3;
+	TA1CCTL2 = OUTMOD_3;
+	P2SEL |= (BIT1|BIT5);
+	P2OUT &=~(BIT3|BIT4);
+}
+void moveRobotBackwards(void)
+{
+	TA1CCR1 = FULL_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_7;
+	TA1CCTL2 = OUTMOD_7;
+	P2SEL |= BIT1|BIT5;
+	P2OUT |= (BIT3|BIT4);
+}
+void moveRobotLeft(void)
+{
+	TA1CCR1 = FULL_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_7;
+	TA1CCTL2 = OUTMOD_3;
+	P2SEL |= BIT1|BIT5;
+	P2OUT |= BIT3;
+	P2OUT &=~BIT4;
+}
+void moveRobotRight(void)
+{
+	TA1CCR1 = FULL_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_3;
+	TA1CCTL2 = OUTMOD_7;
+	P2SEL |= BIT1|BIT5;
+	P2OUT |= BIT4;
+	P2OUT &=~BIT3;
+}
+void moveRobotLeftSlow(void)
+{
+	TA1CCR1 = HALF_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_3;
+	TA1CCTL2 = OUTMOD_3;
+	P2SEL |= BIT1|BIT5;
+	P2OUT &=~BIT4;
+	P2OUT &=~BIT3;
+}
+void moveRobotRightSlow(void)
+{
+	TA1CCR1 = SUPER_FULL_SPEED_LEFT;
+	TA1CCR2 = HALF_SPEED_RIGHT;
+	TA1CCTL1 = OUTMOD_3;
+	TA1CCTL2 = OUTMOD_3;
+	P2SEL |= BIT1|BIT5;
+	P2OUT &=~BIT4;
+	P2OUT &=~BIT3;
+}
+void stopRobot(void)
+{
+	TA1CCR1 = FULL_SPEED_LEFT;
+	TA1CCR2 = FULL_SPEED_RIGHT;
+	P2SEL &=~(BIT1|BIT5);
+	P2OUT &=~(BIT1|BIT3|BIT4|BIT5);
+}
